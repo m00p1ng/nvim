629d299a48df13ca95b0d3ab7e9848468ec77662
m00p1ng <mongkonchai4412@gmail.com>
2022-03-27 23:22:02 +0700

    update git

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -98,6 +98,8 @@ return packer.startup(function(use)
 
   -- Git
   use "lewis6991/gitsigns.nvim"
+  use "ruifm/gitlinker.nvim"
+  use "f-person/git-blame.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
