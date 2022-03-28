435a47e963bcd142559a9d611864ad8965cbf3f3
m00p1ng <mongkonchai4412@gmail.com>
2022-03-29 00:13:02 +0700

    fix thing

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,7 +65,6 @@ return packer.startup(function(use)
   use "Mephistophiles/surround.nvim"
   use "sindrets/diffview.nvim"
   use "ntpeters/vim-better-whitespace"
-  use "tamago324/lir.nvim"
 
   -- Colorschemes
   use "m00p1ng/darkplus.nvim"
