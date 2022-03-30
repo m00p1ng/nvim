48b0c9392b1000fe215920d842090521873767f8
m00p1ng <mongkonchai4412@gmail.com>
2022-03-31 03:42:59 +0700

    remove null-ls

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -106,7 +106,6 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "filipdutescu/renamer.nvim"
   use "ray-x/lsp_signature.nvim"
   use "RRethy/vim-illuminate"
