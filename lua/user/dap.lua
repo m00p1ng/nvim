23851251d758dc511681583ff824986c687b578a
m00p1ng <mongkonchai4412@gmail.com>
2022-04-15 01:01:19 +0700

    add dap config for node

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -64,3 +64,32 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
+
+-- Setup
+
+local installation_path = vim.fn.stdpath("data") .. "/dapinstall"
+
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = { installation_path .. '/jsnode/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require'dap.utils'.pick_process,
+  },
+}
