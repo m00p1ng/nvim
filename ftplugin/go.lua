8ead4cfe32734035a60a48e4e67bd5f0ec76a33b
m00p1ng <mongkonchai4412@gmail.com>
2022-04-23 23:11:13 +0700

    add organize import for go

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -3,12 +3,27 @@ vim.api.nvim_buf_set_option(0, "shiftwidth", 4)
 vim.api.nvim_buf_set_option(0, "softtabstop", 4)
 vim.api.nvim_buf_set_option(0, "expandtab", false)
 
+local function org_imports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
+
 local go_group = vim.api.nvim_create_augroup("_go", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = { "*.go" },
   callback = function()
     vim.lsp.buf.formatting()
+    org_imports(3000)
   end,
   group = go_group,
 })
-
