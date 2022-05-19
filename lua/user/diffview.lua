099c317e8d70aaf3c1508e057d99c89e7428436f
Mongkonchai Priyachiwa <mongkonchai@opn.ooo>
2022-05-19 17:37:17 +0700

    update diffview

--- lua/user/diffview.lua
+++ lua/user/diffview.lua
@@ -18,9 +18,11 @@ configs.setup {
     fold_open = "",
   },
   file_panel = {
-    position = "left",                  -- One of 'left', 'right', 'top', 'bottom'
-    width = 30,                         -- Only applies when position is 'left' or 'right'
-    height = 10,                        -- Only applies when position is 'top' or 'bottom'
+    win_config = {
+      position = "left",                  -- One of 'left', 'right', 'top', 'bottom'
+      width = 30,                         -- Only applies when position is 'left' or 'right'
+      height = 10,                        -- Only applies when position is 'top' or 'bottom'
+    },
     listing_style = "list",             -- One of 'list' or 'tree'
     tree_options = {                    -- Only applies when listing_style is 'tree'
       flatten_dirs = true,              -- Flatten dirs that only contain one single dir
