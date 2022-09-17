cfae53490ccb8a21a71016e346025e2ab737228a
m00p1ng <mongkonchai4412@gmail.com>
2022-09-17 23:52:34 +0700

    update ufo

--- lua/user/ufo.lua
+++ lua/user/ufo.lua
@@ -8,4 +8,47 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.foldnestmax = 2
 
-ufo.setup()
+local ftMap = {
+  vim = { 'treesitter', 'indent' },
+  sh = { 'treesitter', 'indent' },
+  git = { 'treesitter', 'indent' },
+  conf = { 'treesitter', 'indent' },
+  markdown = '',
+
+  alpha = '',
+  NvimTree = '',
+  help = '',
+  packer = '',
+  NeogitStatus = '',
+  NeogitPopup = '',
+  NeogitCommitPopup = '',
+  NeogitCommitMessage = '',
+  DiffviewFiles = '',
+  DiffviewFileHistory = '',
+  Outline = '',
+  qf = '',
+  toggleterm = '',
+  TelescopePrompt = '',
+}
+
+local function customizeSelector(bufnr)
+  local function handleFallbackException(err, providerName)
+    if type(err) == 'string' and err:match('UfoFallbackException') then
+      return ufo.getFolds(providerName, bufnr)
+    else
+      return require('promise').reject(err)
+    end
+  end
+
+  return ufo.getFolds('lsp', bufnr):catch(function(err)
+    return handleFallbackException(err, 'treesitter')
+  end):catch(function(err)
+    return handleFallbackException(err, 'indent')
+  end)
+end
+
+ufo.setup({
+  provider_selector = function(bufnr, filetype, buftype)
+    return ftMap[filetype] or customizeSelector
+  end
+})
