6344cd6e49a18125179290ba23618c48a62c4d7a
m00p1ng <mongkonchai4412@gmail.com>
2022-10-12 23:29:19 +0700

    update icon

--- lua/user/nvim-webdev-icons.lua
+++ lua/user/nvim-webdev-icons.lua
@@ -7,37 +7,61 @@ nvim_web_devicons.set_icon {
   sh = {
     icon = "",
     color = "#1DC123",
-    cterm_color = "59",
     name = "Sh",
   },
   [".gitattributes"] = {
     icon = "",
     color = "#e24329",
-    cterm_color = "59",
     name = "GitAttributes",
   },
   [".gitconfig"] = {
     icon = "",
     color = "#e24329",
-    cterm_color = "59",
     name = "GitConfig",
   },
   [".gitignore"] = {
     icon = "",
     color = "#e24329",
-    cterm_color = "59",
     name = "GitIgnore",
   },
   [".gitlab-ci.yml"] = {
     icon = "",
     color = "#e24329",
-    cterm_color = "166",
     name = "GitlabCI",
   },
   [".gitmodules"] = {
     icon = "",
     color = "#e24329",
-    cterm_color = "59",
     name = "GitModules",
+  },
+  ["spec.ts"] = {
+    icon = "",
+    color = "#FFCC66",
+    name = "TSSPec",
+  },
+  ["test.ts"] = {
+    icon = "",
+    color = "#FFCC66",
+    name = "TSTest",
+  },
+  ["spec.js"] = {
+    icon = "",
+    color = "#FFCC66",
+    name = "JSSpec",
+  },
+  ["test.js"] = {
+    icon = "",
+    color = "#FFCC66",
+    name = "JSTest",
+  },
+  ["dockerignore"] = {
+    icon = "",
+    color = "#384d54",
+    name = "DockerIgnore",
+  },
+  ["docker-compose.yml"] = {
+    icon = "",
+    color = "#384d54",
+    name = "DockerCompoose",
   },
 }
