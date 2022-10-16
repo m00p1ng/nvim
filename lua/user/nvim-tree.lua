70ae130da0983ac6a0ae5d1f93e216ea8cb0b9e6
m00p1ng <mongkonchai4412@gmail.com>
2022-10-16 20:15:14 +0700

    update nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -8,64 +8,37 @@ local icons = require "user.icons"
 local tree_cb = require 'nvim-tree.config'.nvim_tree_callback
 
 nvim_tree.setup {
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "alpha",
-  },
-  open_on_tab = false,
+  auto_reload_on_write = true,
+  create_in_closed_folder = false,
+  disable_netrw = false,
   hijack_cursor = false,
-  update_cwd = false,
-  diagnostics = {
-    enable = true,
-    show_on_dirs = false,
-    icons = {
-      hint = icons.diagnostics.Hint,
-      info = icons.diagnostics.Information,
-      warning = icons.diagnostics.Warning,
-      error = icons.diagnostics.Error,
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  filters = {
-    dotfiles = false,
-    custom = {
-      '\\.git$',
-      '\\.DS_Store',
-    },
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 400,
-  },
-  actions = {
-    open_file = {
-      window_picker = {
-        exclude = {
-          filetype = {
-            "notify",
-            "packer",
-            "qf",
-            "diff",
-            "fugitive",
-            "fugitiveblame",
-            "dap-repl",
-          },
-        },
-      },
-    },
-  },
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  ignore_buffer_on_setup = false,
+  open_on_setup = false,
+  open_on_setup_file = false,
+  open_on_tab = false,
+  focus_empty_on_setup = false,
+  ignore_buf_on_tab_change = {},
+  sort_by = "name",
+  root_dirs = {},
+  prefer_startup_root = false,
+  sync_root_with_cwd = false,
+  reload_on_bufenter = false,
+  respect_buf_cwd = false,
+  on_attach = "disable",
+  remove_keymaps = false,
+  select_prompts = false,
   view = {
+    adaptive_size = false,
+    centralize_selection = false,
     width = 30,
     hide_root_folder = false,
     side = "left",
+    preserve_window_proportions = false,
     number = false,
     relativenumber = false,
+    signcolumn = "yes",
     mappings = {
       custom_only = false,
       list = {
@@ -73,12 +46,37 @@ nvim_tree.setup {
         { key = "2", cb = tree_cb "prev_git_item" },
       },
     },
+    float = {
+      enable = false,
+      quit_on_focus_loss = true,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = 30,
+        height = 30,
+        row = 1,
+        col = 1,
+      },
+    },
   },
   renderer = {
+    add_trailing = false,
+    group_empty = false,
     highlight_git = true,
+    full_name = false,
+    highlight_opened_files = "none",
     root_folder_modifier = ":t",
+    indent_width = 2,
     indent_markers = {
       enable = true,
+      inline_arrows = true,
+      icons = {
+        corner = "└",
+        edge = "│",
+        item = "│",
+        bottom = "─",
+        none = " ",
+      },
     },
     icons = {
       webdev_colors = true,
@@ -92,9 +90,12 @@ nvim_tree.setup {
         git = true,
       },
       glyphs = {
-        default = icons.documents.File,
+        default = "",
         symlink = icons.documents.Symlink,
+        bookmark = icons.ui.BookMark,
         folder = {
+          arrow_closed = icons.ui.ArrowClosed,
+          arrow_open = icons.ui.ArrowOpen,
           default = icons.documents.Folder,
           open = icons.documents.OpenFolder,
           empty = icons.documents.EmptyFolder,
@@ -112,6 +113,119 @@ nvim_tree.setup {
           ignored = "◌",
         },
       },
+    },
+    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    symlink_destination = true,
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+    ignore_list = {},
+  },
+  ignore_ft_on_setup = {
+    "alpha",
+  },
+  system_open = {
+    cmd = "",
+    args = {},
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = false,
+    debounce_delay = 50,
+    icons = {
+      hint = icons.diagnostics.Hint,
+      info = icons.diagnostics.Information,
+      warning = icons.diagnostics.Warning,
+      error = icons.diagnostics.Error,
+    },
+  },
+  filters = {
+    dotfiles = false,
+    custom = {
+      '\\.git$',
+      '\\.DS_Store',
+    },
+    exclude = {},
+  },
+  filesystem_watchers = {
+    enable = true,
+    debounce_delay = 50,
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    show_on_dirs = true,
+    timeout = 400,
+  },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = true,
+      global = false,
+      restrict_above_cwd = false,
+    },
+    expand_all = {
+      max_folder_discovery = 300,
+      exclude = {},
+    },
+    file_popup = {
+      open_win_config = {
+        col = 1,
+        row = 1,
+        relative = "cursor",
+        border = "shadow",
+        style = "minimal",
+      },
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = true,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = {
+            "notify",
+            "packer",
+            "qf",
+            "diff",
+            "fugitive",
+            "fugitiveblame",
+            "dap-repl",
+          },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+    remove_file = {
+      close_window = true,
+    },
+  },
+  trash = {
+    cmd = "gio trash",
+    require_confirm = true,
+  },
+  live_filter = {
+    prefix = "[FILTER]: ",
+    always_show_folders = true,
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      dev = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
+      watcher = false,
     },
   },
 }
