7a87426d951e2958ccea44ffd9b8bc3520e39b4b
m00p1ng <mongkonchai4412@gmail.com>
2022-11-06 18:56:51 +0700

    update

--- lua/user/whitespace.lua
+++ lua/user/whitespace.lua
@@ -1,9 +1,5 @@
 vim.g.better_whitespace_enabled = 1
-vim.g.strip_whitespace_on_save = 1
+vim.g.strip_whitespace_on_save = 0
 vim.g.strip_whitespace_confirm = 0
 vim.g.strip_only_modified_lines = 1
-vim.g.better_whitespace_filetypes_blacklist = {
-  "alpha",
-  "NvimTree",
-  "toggleterm",
-}
+vim.g.better_whitespace_filetypes_blacklist = require('user.function').ui_filetypes
