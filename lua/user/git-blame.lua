bda92f37914613f10045f06c36c8a79f35431c8e
m00p1ng <mongkonchai4412@gmail.com>
2022-11-21 10:07:53 +0700

    update git blame

--- lua/user/git-blame.lua
+++ lua/user/git-blame.lua
@@ -1,6 +1,7 @@
 vim.g.gitblame_enabled = 1
 vim.g.gitblame_date_format = '%r'
 vim.g.gitblame_message_template = "   <author>, <date> • <summary>"
+vim.g.gitblame_message_when_not_committed = "  You, now • Uncommitted changes"
 vim.g.gitblame_highlight_group = "GitBlame"
 vim.g.gitblame_ignored_filetypes = require('user.function').ui_filetypes
 vim.g.gitblame_set_extmark_options = {
