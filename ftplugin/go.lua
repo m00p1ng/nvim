ea98a5559d6019e71fa2063f1def8110ab08d9d5
m00p1ng <mongkonchai4412@gmail.com>
2023-02-03 16:15:20 +0700

    add dap go

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -27,3 +27,41 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
   group = go_group,
 })
+
+local dap_status_ok, dap = pcall(require, "dap")
+if not dap_status_ok then
+  return
+end
+
+local installation_path = vim.fn.stdpath("data") .. "/mason/packages"
+
+dap.adapters.delve = {
+  type = 'server',
+  port = '${port}',
+  executable = {
+    command = installation_path .. '/delve/dlv',
+    args = {'dap', '-l', '127.0.0.1:${port}'},
+  }
+}
+
+dap.configurations.go = {
+  {
+    type = "delve",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+  {
+    type = "delve",
+    name = "Debug Package",
+    request = "launch",
+    program = "${fileDirname}",
+  },
+  {
+    type = "delve",
+    name = "Attach",
+    mode = "local",
+    request = "attach",
+    processId = require("dap.utils").pick_process,
+  },
+}
