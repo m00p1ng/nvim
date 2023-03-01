965b7fa31983fb2de093fe11838aee5d39d46a9c
m00p1ng <mongkonchai4412@gmail.com>
2023-03-01 13:07:49 +0700

    update tailwindcss

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -36,7 +36,9 @@ return {
       },
       -- LSP Server Settings
       ---@type lspconfig.options
-      servers = {},
+      servers = {
+        tailwindcss = require("plugins.lsp.lang.tailwindcss").opts(),
+      },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
---
+++ lua/plugins/lsp/lang/tailwindcss.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.opts()
+  return {
+    root_dir = function(fname)
+      local util = require "lspconfig/util"
+      return util.root_pattern("tailwind.config.js", "tailwind.config.cjs", "tailwind.js", "tailwind.cjs")(fname)
+    end,
+  }
+end
+
+return M
