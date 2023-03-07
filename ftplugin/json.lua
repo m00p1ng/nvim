b153922444792720c47c62842ca7cf3f14b650aa
m00p1ng <mongkonchai4412@gmail.com>
2023-03-07 12:01:37 +0700

    update package.json

---
+++ ftplugin/json.lua
@@ -0,0 +1,26 @@
+local filename = vim.fn.expand "%:t"
+
+if filename == "package.json" then
+  local which_key = require "which-key"
+  local opts = {
+    mode = "n", -- NORMAL mode
+    prefix = "<leader>",
+    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true, -- use `silent` when creating keymaps
+    noremap = true, -- use `noremap` when creating keymaps
+    nowait = true, -- use `nowait` when creating keymaps
+  }
+
+  local mappings = {
+    m = {
+      name = "Package.json",
+      t = { "<cmd>lua require('package-info').toggle()<cr>", "Toggle" },
+      u = { "<cmd>lua require('package-info').update()<cr>", "Update" },
+      d = { "<cmd>lua require('package-info').delete()<cr>", "Delete" },
+      i = { "<cmd>lua require('package-info').install()<cr>", "Install" },
+      c = { "<cmd>lua require('package-info').change_version()<cr>", "Change" },
+    },
+  }
+
+  which_key.register(mappings, opts)
+end
--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,6 +1,5 @@
 return {
   "vuki656/package-info.nvim",
-  enabled = false,
   opts = {
     colors = {
       up_to_date = "#3C4048", -- Text color for up to date dependency virtual text
@@ -13,9 +12,9 @@ return {
         outdated = "|  ", -- Icon for outdated dependencies
       },
     },
-    autostart = true, -- Whether to autostart when `package.json` is opened
-    hide_up_to_date = false, -- It hides up to date versions when displaying virtual text
-    hide_unstable_versions = false, -- It hides unstable versions from version list e.g next-11.1.3-canary3
+    autostart = false, -- Whether to autostart when `package.json` is opened
+    hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
+    hide_unstable_versions = true, -- It hides unstable versions from version list e.g next-11.1.3-canary3
     -- Can be `npm`, `yarn`, or `pnpm`. Used for `delete`, `install` etc...
     -- The plugin will try to auto-detect the package manager based on
     -- `yarn.lock` or `package-lock.json`. If none are found it will use the
