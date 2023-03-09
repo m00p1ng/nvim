c124346a62896c40410e62ac24fecd6d943e050c
m00p1ng <mongkonchai4412@gmail.com>
2023-03-09 16:32:38 +0700

    add last-loc

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -59,3 +59,14 @@ autocmd("User", {
     require("utils.winbar").create_winbar()
   end,
 })
+
+autocmd("BufReadPost", {
+  group = augroup("last_loc", { clear = true}),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
