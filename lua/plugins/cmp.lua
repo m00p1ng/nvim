35666ebed2f9d54e515c6ef0de620ceae59f29ed
m00p1ng <mongkonchai4412@gmail.com>
2023-03-09 20:42:57 +0700

    remove unused

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -59,7 +59,7 @@ return {
             cmp.select_next_item()
           elseif luasnip.expandable() then
             luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
+          elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           else
             fallback()
@@ -150,15 +150,10 @@ return {
           compare.order,
         },
       },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
       experimental = {
         ghost_text = {
           hl_group = "CmpGhostText",
         },
-        native_menu = false,
       },
     }
 
@@ -215,13 +210,13 @@ return {
       },
     })
 
-    vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-      group = vim.api.nvim_create_augroup("_fix_luasnaip", { clear = true }),
-      callback = function()
-        if luasnip.expand_or_jumpable() then
-          luasnip.unlink_current()
-        end
-      end,
-    })
+    -- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    --   group = vim.api.nvim_create_augroup("fix_luasnaip", { clear = true }),
+    --   callback = function()
+    --     if luasnip.expand_or_jumpable() then
+    --       luasnip.unlink_current()
+    --     end
+    --   end,
+    -- })
   end,
 }
