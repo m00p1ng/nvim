d097b25b62b0c00f98fb714b522dd72c63075dcf
m00p1ng <mongkonchai4412@gmail.com>
2023-03-20 11:19:14 +0700

    update

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_buf_set_option(0, "tabstop", 4)
+vim.api.nvim_buf_set_option(0, "shiftwidth", 4)
+vim.api.nvim_buf_set_option(0, "softtabstop", 4)
+vim.api.nvim_buf_set_option(0, "expandtab", false)
