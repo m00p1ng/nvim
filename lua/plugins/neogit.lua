255298d32723e58953b763672e77e6df5d44a201
m00p1ng <mongkonchai4412@gmail.com>
2023-03-22 05:23:18 +0700

    update neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -9,12 +9,24 @@ return {
     disable_context_highlighting = false,
     disable_commit_confirmation = true,
     auto_refresh = true,
+    sort_branches = "-committerdate",
     disable_builtin_notifications = false,
     use_magit_keybindings = false,
+    kind = "tab",
+    console_timeout = 2000,
+    auto_show_console = true,
+    remember_settings = true,
+    use_per_project_settings = true,
+    ignored_settings = {},
     commit_popup = {
       kind = "split",
     },
-    kind = "tab",
+    preview_buffer = {
+      kind = "split",
+    },
+    popup = {
+      kind = "split",
+    },
     signs = {
       -- { CLOSED, OPENED }
       section = { icons.ui.ArrowClosed, icons.ui.ArrowOpen },
