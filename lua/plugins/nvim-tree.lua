ca39ace30742e6065333e43418b14d67ed8781bd
m00p1ng <mongkonchai4412@gmail.com>
2023-04-02 13:53:37 +0700

    update

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -118,11 +118,10 @@ return {
     update_focused_file = {
       enable = true,
       update_root = true,
-      ignore_list = vim.tbl_deep_extend(
-        "force",
-        require('utils').ui_filetypes,
-        { "/nix/store", "~/.config", }
-      ),
+      ignore_list = {
+        "/nix/store",
+        "~/.config",
+      },
     },
     ignore_ft_on_setup = {},
     system_open = {
