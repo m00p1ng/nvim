1595b379fb37a8e5503d476f82874007c03df2c5
m00p1ng <mongkonchai4412@gmail.com>
2023-09-03 00:27:01 +0700

    add rainbow delimiter

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,22 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  config = function()
+    local rainbow_delimiters = require "rainbow-delimiters"
+
+    vim.g.rainbow_delimiters = {
+      strategy = {
+        [""] = rainbow_delimiters.strategy["global"],
+        vim = rainbow_delimiters.strategy["local"],
+      },
+      query = {
+        [""] = "rainbow-delimiters",
+        -- lua = "rainbow-blocks",
+      },
+      highlight = {
+        "RainbowDelimiter1",
+        "RainbowDelimiter2",
+        "RainbowDelimiter3",
+      },
+    }
+  end,
+}
