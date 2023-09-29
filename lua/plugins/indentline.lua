eb08e41e0a5d353f5a82543a77396a374858fc1d
m00p1ng <mongkonchai4412@gmail.com>
2023-09-29 14:17:07 +0700

    update indenline

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,16 +1,62 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPost", "BufNewFile" },
+  main = "ibl",
   opts = {
-    -- char = "│"
-    -- char = "▎"
-    char = "▏",
-    context_char = "▏",
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
-    use_treesitter = true,
-    show_current_context = true,
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = require("utils").ui_filetypes,
+    enabled = true,
+    debounce = 200,
+    viewport_buffer = {
+      min = 30,
+      max = 500,
+    },
+    indent = {
+      -- char = "│"
+      -- char = "▎"
+      char = "▏",
+      tab_char = nil,
+      highlight = "IblIndent",
+      smart_indent_cap = true,
+      priority = 1,
+    },
+    whitespace = {
+      highlight = "IblWhitespace",
+      remove_blankline_trail = true,
+    },
+    scope = {
+      enabled = true,
+      char = "▏",
+      show_start = true,
+      show_end = true,
+      injected_languages = true,
+      highlight = "IblScope",
+      priority = 1024,
+      include = {
+        node_type = {},
+      },
+      exclude = {
+        language = {},
+        node_type = {
+          ["*"] = {
+            "source_file",
+            "program",
+          },
+          lua = {
+            "chunk",
+          },
+          python = {
+            "module",
+          },
+        },
+      },
+    },
+    exclude = {
+      filetypes = require("utils").ui_filetypes,
+      buftypes = {
+        "terminal",
+        "nofile",
+        "quickfix",
+        "prompt",
+      },
+    },
   },
 }
