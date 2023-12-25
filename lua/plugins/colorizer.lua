d76d87e94328d0a75de312c002c02d49bcfe2d81
m00p1ng <mongkonchai4412@gmail.com>
2023-12-25 17:10:03 +0700

    update colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,20 +1,35 @@
 return {
   "NvChad/nvim-colorizer.lua",
-  event = { "BufReadPost", "BufNewFile" },
+  -- event = { "BufReadPost", "BufNewFile" },
   cond = vim.g.vscode == nil,
   opts = {
-    filetypes = { "*" },
+    filetypes = {
+      "*",
+
+      -- Excluded filteypes.
+      "!lazy", -- Commit hashes get highlighted sometimes.
+    },
     user_default_options = {
       RGB = true, -- #RGB hex codes
       RRGGBB = true, -- #RRGGBB hex codes
-      names = false, -- "Name" codes like Blue oe blue
+      names = false, -- "Name" codes like Blue or blue
       RRGGBBAA = true, -- #RRGGBBAA hex codes
+      AARRGGBB = false, -- 0xAARRGGBB hex codes
       rgb_fn = true, -- CSS rgb() and rgba() functions
       hsl_fn = true, -- CSS hsl() and hsla() functions
-      css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      -- Available modes: foreground, background, virtualtext
-      mode = "background", -- Set the display mode.)
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      -- Available modes for `mode`: foreground, background,  virtualtext
+      mode = "background", -- Set the display mode.
+      -- Available methods are false / true / "normal" / "lsp" / "both"
+      -- True is same as normal
+      tailwind = false, -- Enable tailwind colors
+      -- parsers can contain values used in |user_default_options|
+      sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+      virtualtext = "■",
+      -- update color values even if buffer is not focused
+      -- example use: cmp_menu, cmp_docs
+      always_update = false,
     },
   },
 }
