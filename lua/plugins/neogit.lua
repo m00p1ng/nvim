c97b24b02553e60d56401bc6eb24d52310bf3d33
m00p1ng <mongkonchai4412@gmail.com>
2024-05-19 02:33:17 +0700

    update neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -11,19 +11,24 @@ return {
     disable_context_highlighting = false,
     -- Disables signs for sections/items/hunks
     disable_signs = false,
-    -- Do not ask to confirm the commit - just do it when the buffer is closed.
-    disable_commit_confirmation = false,
-    -- Uses `vim.notify` instead of the built-in notification system.
-    disable_builtin_notifications = false,
     -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
     -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
     -- normal mode.
-    disable_insert_on_commit = true,
+    disable_insert_on_commit = "auto",
     -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
     -- events.
     filewatcher = {
       interval = 1000,
       enabled = true,
+    },
+    -- "ascii"   is the graph the git CLI generates
+    -- "unicode" is the graph like https://github.com/rbong/vim-flog
+    graph_style = "unicode",
+    -- Used to generate URL's for branch popup action "pull request".
+    git_services = {
+      ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+      ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+      ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
     },
     -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
     -- sorter instead. By default, this function returns `nil`.
@@ -42,6 +47,14 @@ return {
       "NeogitCommitPopup--allow-empty",
       "NeogitRevertPopup--no-edit",
     },
+    -- Configure highlight group features
+    highlight = {
+      italic = true,
+      bold = true,
+      underline = true,
+    },
+    -- Set to false if you want to be responsible for creating _ALL_ keymappings
+    use_default_keymaps = true,
     -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
     -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
     auto_refresh = true,
@@ -59,29 +72,59 @@ return {
     -- Automatically show console if a command takes more than console_timeout milliseconds
     auto_show_console = true,
     status = {
+      show_head_commit_hash = true,
       recent_commit_count = 10,
+      HEAD_padding = 10,
+      mode_padding = 3,
+      mode_text = {
+        M = "modified",
+        N = "new file",
+        A = "added",
+        D = "deleted",
+        C = "copied",
+        U = "updated",
+        R = "renamed",
+        DD = "unmerged",
+        AU = "unmerged",
+        UD = "unmerged",
+        UA = "unmerged",
+        DU = "unmerged",
+        AA = "unmerged",
+        UU = "unmerged",
+        ["?"] = "",
+      },
     },
     commit_editor = {
       kind = "split",
+      show_staged_diff = false,
+      -- Accepted values:
+      -- "split" to show the staged diff below the commit editor
+      -- "vsplit" to show it to the right
+      -- "split_above" Like :top split
+      -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
+      staged_diff_split_kind = "split",
     },
     commit_select_view = {
       kind = "tab",
     },
     commit_view = {
       kind = "vsplit",
-      verify_commit = os.execute "which gpg" == 0, -- Can be set to true or false, otherwise we try to find the binary
+      verify_commit = vim.fn.executable "gpg" == 1, -- Can be set to true or false, otherwise we try to find the binary
     },
     log_view = {
       kind = "tab",
     },
     rebase_editor = {
-      kind = "split",
+      kind = "auto",
     },
     reflog_view = {
       kind = "tab",
     },
     merge_editor = {
-      kind = "split",
+      kind = "auto",
+    },
+    tag_editor = {
+      kind = "auto",
     },
     preview_buffer = {
       kind = "split",
@@ -159,6 +202,37 @@ return {
       },
     },
     mappings = {
+      commit_editor = {
+        ["q"] = "Close",
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+      },
+      commit_editor_I = {
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+      },
+      rebase_editor = {
+        ["p"] = "Pick",
+        ["r"] = "Reword",
+        ["e"] = "Edit",
+        ["s"] = "Squash",
+        ["f"] = "Fixup",
+        ["x"] = "Execute",
+        ["d"] = "Drop",
+        ["b"] = "Break",
+        ["q"] = "Close",
+        ["<cr>"] = "OpenCommit",
+        ["gk"] = "MoveUp",
+        ["gj"] = "MoveDown",
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+        ["[c"] = "OpenOrScrollUp",
+        ["]c"] = "OpenOrScrollDown",
+      },
+      rebase_editor_I = {
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+      },
       finder = {
         ["<cr>"] = "Select",
         ["<c-c>"] = "Close",
@@ -171,6 +245,7 @@ return {
         ["<s-tab>"] = "MultiselectTogglePrevious",
         ["<c-j>"] = "NOP",
       },
+      -- Setting any of these to `false` will disable the mapping.
       popup = {
         ["?"] = "HelpPopup",
         ["A"] = "CherryPickPopup",
@@ -179,9 +254,8 @@ return {
         ["P"] = "PushPopup",
         ["X"] = "ResetPopup",
         ["Z"] = "StashPopup",
-        ["i"] = "IgnorePopup",
-        ["t"] = "TagPopup",
         ["b"] = "BranchPopup",
+        ["B"] = "BisectPopup",
         ["c"] = "CommitPopup",
         ["f"] = "FetchPopup",
         ["l"] = "LogPopup",
@@ -189,8 +263,8 @@ return {
         ["p"] = "PullPopup",
         ["r"] = "RebasePopup",
         ["v"] = "RevertPopup",
+        ["w"] = "WorktreePopup",
       },
-      -- Setting any of these to `false` will disable the mapping.
       status = {
         ["q"] = "Close",
         ["I"] = "InitRepo",
@@ -203,10 +277,11 @@ return {
         ["s"] = "Stage",
         ["S"] = "StageUnstaged",
         ["<c-s>"] = "StageAll",
+        ["K"] = "Untrack",
         ["u"] = "Unstage",
         ["U"] = "UnstageStaged",
         ["$"] = "CommandHistory",
-        ["#"] = "Console",
+        ["Y"] = "YankSelected",
         ["<c-r>"] = "RefreshBuffer",
         ["<enter>"] = "Toggle",
         ["<c-v>"] = "VSplitOpen",
@@ -214,6 +289,8 @@ return {
         ["<c-t>"] = "TabOpen",
         ["{"] = "GoToPreviousHunkHeader",
         ["}"] = "GoToNextHunkHeader",
+        ["[c"] = "OpenOrScrollUp",
+        ["]c"] = "OpenOrScrollDown",
       },
     },
   },
