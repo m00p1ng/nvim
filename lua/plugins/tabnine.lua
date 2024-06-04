8e9064fc7ae03d4ce6b1921d9e2361f2572674aa
m00p1ng <mongkonchai4412@gmail.com>
2024-06-04 13:47:06 +0700

    update tabnine

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -26,8 +26,10 @@ return {
       debounce_ms = 800,
       suggestion_color = { gui = "#b668cd", cterm = 244 },
       codelens_color = { gui = "#b668cd", cterm = 244 },
-      codelens_enabled = true,
+      codelens_enabled = false,
       exclude_filetypes = require("utils").ui_filetypes,
+      log_file_path = nil,
+      tabnine_enterprise_host = nil,
     },
     config = function(_, opts)
       vim.api.nvim_set_hl(0, "TabnineSuggestion", { fg = opts.suggestion_color.gui })
