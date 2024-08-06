b9b24b220793906d7d5acfc7eaffe7e3ba9c8139
m00p1ng <mongkonchai4412@gmail.com>
2024-08-06 12:25:44 +0700

    update

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -4,9 +4,9 @@ return {
     disable_on_zoom = true,
   },
   keys = {
-    { "<C-h>", ":lua require('Navigator').left()<cr>", noremap = true, silent = true, desc = "TmuxLeft" },
-    { "<C-j>", ":lua require('Navigator').down()<cr>", noremap = true, silent = true, desc = "TmuxDown" },
-    { "<C-k>", ":lua require('Navigator').up()<cr>", noremap = true, silent = true, desc = "TmuxUp" },
-    { "<C-l>", ":lua require('Navigator').right()<cr>", noremap = true, silent = true, desc = "TmuxRight" },
+    { "<C-h>", ":lua require('Navigator').left()<cr>", desc = "Tmux Left" },
+    { "<C-j>", ":lua require('Navigator').down()<cr>", desc = "Tmux Down" },
+    { "<C-k>", ":lua require('Navigator').up()<cr>", desc = "Tmux Up" },
+    { "<C-l>", ":lua require('Navigator').right()<cr>", desc = "Tmux Right" },
   },
 }
