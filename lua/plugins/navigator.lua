ab0d77c7635af7676ca4550b683969e3e533dd46
m00p1ng <mongkonchai4412@gmail.com>
2024-12-28 13:15:07 +0700

    fix navigator

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -4,9 +4,9 @@ return {
     disable_on_zoom = true,
   },
   keys = {
-    { "<C-h>", ":lua require('Navigator').left()<cr>", desc = "Tmux Left" },
-    { "<C-j>", ":lua require('Navigator').down()<cr>", desc = "Tmux Down" },
-    { "<C-k>", ":lua require('Navigator').up()<cr>", desc = "Tmux Up" },
-    { "<C-l>", ":lua require('Navigator').right()<cr>", desc = "Tmux Right" },
+    { "<C-h>", "<cmd>lua require('Navigator').left()<cr>", desc = "Tmux Left" },
+    { "<C-j>", "<cmd>lua require('Navigator').down()<cr>", desc = "Tmux Down" },
+    { "<C-k>", "<cmd>lua require('Navigator').up()<cr>", desc = "Tmux Up" },
+    { "<C-l>", "<cmd>lua require('Navigator').right()<cr>", desc = "Tmux Right" },
   },
 }
