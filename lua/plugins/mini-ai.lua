48de98f9f9811679ab1e0771f91b1ae0f54c3309
m00p1ng <mongkonchai4412@gmail.com>
2025-01-02 20:15:18 +0700

    update mini.ai

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -1,4 +1,9 @@
 return {
   "echasnovski/mini.ai",
-  opts = {},
+  opts = {
+    -- How to search for object (first inside current line, then inside
+    -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+    -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+    search_method = "cover",
+  },
 }
