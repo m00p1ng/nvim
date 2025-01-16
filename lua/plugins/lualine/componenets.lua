2e1504b021f66344646838bc29dca64b94ed390f
m00p1ng <mongkonchai4412@gmail.com>
2025-01-16 14:03:52 +0700

    extract lualine

---
+++ lua/plugins/lualine/componenets.lua
@@ -0,0 +1,277 @@
+M = {}
+
+local f = require "utils"
+local dark = "#181825"
+
+vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#fab387", bg = dark })
+vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#bac2de", bg = dark, bold = false })
+vim.api.nvim_set_hl(0, "SLLocation", { fg = "#89b4fa", bg = dark })
+vim.api.nvim_set_hl(0, "SLFiletype", { fg = "#89dceb", bg = dark })
+vim.api.nvim_set_hl(0, "SLIndent", { fg = "#a6e3a1", bg = dark })
+vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#6c7086", bg = dark, italic = true })
+vim.api.nvim_set_hl(0, "SLError", { fg = "#f38ba8", bg = dark })
+vim.api.nvim_set_hl(0, "SLWarning", { fg = "#f9e2af", bg = dark })
+vim.api.nvim_set_hl(0, "SLFileSize", { fg = "#bac2de", bg = dark })
+vim.api.nvim_set_hl(0, "SLTabs", { fg = "#a6e3a1", bg = dark })
+
+local hl_str = function(str, hl)
+  return "%#" .. hl .. "#" .. str
+end
+
+local mode_color = {
+  n = "#89b4fa",
+  i = "#fab387",
+  v = "#cba6f7",
+  [""] = "#cba6f7",
+  V = "#cba6f7",
+  -- c      = '#b5cea8',
+  -- c      = '#f9e2af',
+  c = "#74c7ec",
+  no = "#eba0ac",
+  s = "#a6e3a1",
+  S = "#fab387",
+  [""] = "#fab387",
+  ic = "#f38ba8",
+  R = "#eba0ac",
+  Rv = "#f38ba8",
+  cv = "#89b4fa",
+  ce = "#89b4fa",
+  r = "#f38ba8",
+  rm = "#74c7ec",
+  ["r?"] = "#74c7ec",
+  ["!"] = "#74c7ec",
+  t = "#f38ba8",
+}
+
+local icons = require "utils.icons"
+
+M.diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = {
+    error = "%#SLError#" .. icons.diagnostics.Error .. " ",
+    warn = "%#SLWarning#" .. icons.diagnostics.Warning .. " ",
+  },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
+}
+
+M.mode = {
+  function()
+    return "▌"
+  end,
+  color = function()
+    return { fg = mode_color[vim.fn.mode()], bg = "NONE" }
+  end,
+  padding = 0,
+}
+
+M.filetype = {
+  "filetype",
+  fmt = function(str)
+    local return_val = function(str)
+      return hl_str(str, "SLFiletype")
+    end
+
+    if str == "TelescopePrompt" then
+      return return_val(icons.ui.Telescope)
+    end
+
+    if f.is_ui_filetype(str) or str == "" then
+      return ""
+    else
+      return return_val(str)
+    end
+  end,
+  icons_enabled = false,
+  on_click = function()
+    vim.cmd.Telescope "filetypes"
+  end,
+}
+
+M.branch = {
+  "branch",
+  icons_enabled = true,
+  icon = "%#SLGitIcon#" .. icons.git.Branch .. "%*" .. "%#SLBranchName#",
+  colored = false,
+  fmt = function(str)
+    if str == "" or str == nil then
+      return "[unknown]"
+    end
+
+    local max_length = 30
+    if str:len() > max_length then
+      return str:sub(1, max_length) .. "…"
+    end
+
+    return str
+  end,
+  on_click = function()
+    vim.cmd.Telescope "git_branches"
+  end,
+}
+
+-- local gstatus = { ahead = 0, behind = 0 }
+-- local function update_gstatus()
+--   local Job = require "plenary.job"
+--   Job:new({
+--     command = "git",
+--     args = { "rev-list", "--left-right", "--count", "HEAD...@{upstream}" },
+--     on_exit = function(job, _)
+--       local res = job:result()[1]
+--       if type(res) ~= "string" then
+--         gstatus = { ahead = 0, behind = 0 }
+--         return
+--       end
+--       local ok, ahead, behind = pcall(string.match, res, "(%d+)%s*(%d+)")
+--       if not ok then
+--         ahead, behind = 0, 0
+--       end
+--       gstatus = { ahead = tonumber(ahead), behind = tonumber(behind) }
+--     end,
+--   }):start()
+-- end
+--
+-- if _G.Gstatus_timer == nil then
+--   _G.Gstatus_timer = vim.loop.new_timer()
+-- else
+--   _G.Gstatus_timer:stop()
+-- end
+-- _G.Gstatus_timer:start(0, 2000, vim.schedule_wrap(update_gstatus))
+--
+-- local remote_status = {
+--   function()
+--     local result = ""
+--     if gstatus.ahead ~= 0 then
+--       result = "" .. gstatus.ahead
+--     end
+--     if gstatus.behind ~= 0 then
+--       result = "" .. gstatus.behind
+--     end
+--
+--     return hl_str(result, "SLFilesize")
+--   end,
+--   cond = function()
+--     return gstatus.ahead ~= 0 or gstatus.behind ~= 0
+--   end,
+-- }
+
+M.spaces = {
+  function()
+    local ft = vim.bo.ft
+    if f.is_ui_filetype(ft) then
+      return ""
+    end
+
+    local shiftwidth = vim.bo.shiftwidth
+    if shiftwidth == nil then
+      return ""
+    end
+
+    return hl_str(icons.ui.Tab .. shiftwidth, "SLIndent")
+  end,
+}
+
+M.location = {
+  "location",
+  fmt = function(str)
+    return hl_str(str, "SLLocation")
+  end,
+}
+
+M.filesize = {
+  function()
+    local ft = vim.bo.ft
+    if f.is_ui_filetype(ft) then
+      return ""
+    end
+
+    local size = vim.fn.wordcount().bytes
+
+    local suffixes = { "B", "KB", "MB", "GB" }
+
+    local i = 1
+    while size > 1024 and i < #suffixes do
+      size = size / 1024
+      i = i + 1
+    end
+
+    local format = i == 1 and "%d%s" or "%.1f%s"
+    return hl_str(string.format(format, size, suffixes[i]), "SLFilesize")
+  end,
+}
+
+M.tabs = {
+  function()
+    local num_tabs = #vim.api.nvim_list_tabpages()
+
+    if num_tabs > 1 then
+      local tabpage_number = tostring(vim.api.nvim_tabpage_get_number(0))
+      return hl_str(" " .. tabpage_number .. "/" .. tostring(num_tabs), "SLTabs")
+    end
+
+    return ""
+  end,
+}
+
+-- recording @q
+M.status_mode = {
+  require("noice").api.status.mode.get,
+  cond = require("noice").api.status.mode.has,
+}
+
+M.search_result = {
+  require("noice").api.status.search.get,
+  cond = require("noice").api.status.search.has,
+}
+
+M.command = {
+  require("noice").api.status.command.get,
+  cond = require("noice").api.status.command.has,
+  color = { fg = "#eba0ac" },
+}
+
+M.autoformat = {
+  function()
+    if not vim.g.autoformat then
+      return "G"
+    end
+
+    if vim.b.autoformat ~= nil and not vim.b.autoformat then
+      return "B"
+    end
+  end,
+  icon = "󱏟",
+  cond = function()
+    return not vim.g.autoformat or (vim.b.autoformat ~= nil and not vim.b.autoformat)
+  end,
+}
+
+M.updated_plugin = {
+  require("lazy.status").updates,
+  cond = require("lazy.status").has_updates,
+  color = { fg = "Special", bg = "NONE" },
+}
+
+M.current_signature = {
+  function()
+    local ft = vim.bo.ft
+    if ft == "TelescopePrompt" then
+      return ""
+    end
+
+    local sig = require("lsp_signature").status_line(30)
+    local hint = sig.hint
+
+    if not require("utils").is_empty(hint) then
+      return "%#SLSeparator# " .. hint .. "%*"
+    end
+
+    return ""
+  end,
+  padding = 0,
+}
+
+return M
