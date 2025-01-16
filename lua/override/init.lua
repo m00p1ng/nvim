cc2b0bb3cca1e04d8fb0ba89ca2d81e92bca8d54
m00p1ng <mongkonchai4412@gmail.com>
2025-01-17 00:16:59 +0700

    update override

--- lua/override/init.lua
+++ lua/override/init.lua
@@ -1,1 +1,13 @@
-return {}
+return {
+  -- { import = "plugins.lang.docker" },
+  -- { import = "plugins.lang.go" },
+  -- { import = "plugins.lang.json" },
+  -- { import = "plugins.lang.lua" },
+  -- { import = "plugins.lang.markdown" },
+  -- { import = "plugins.lang.nix" },
+  -- { import = "plugins.lang.python" },
+  -- { import = "plugins.lang.tailwind" },
+  -- { import = "plugins.lang.typescript" },
+  -- { import = "plugins.lang.vue" },
+  -- { import = "plugins.lang.yaml" },
+}
