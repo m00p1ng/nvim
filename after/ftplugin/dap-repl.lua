22390a17baf341539ad3817bc0370e54b8d67b8c
m00p1ng <mongkonchai4412@gmail.com>
2025-01-18 00:04:41 +0700

    update dap

--- after/ftplugin/dap-repl.lua
+++
@@ -1,5 +0,0 @@
-local opts = { noremap = true, silent = true, buffer = true }
-vim.keymap.set("i", "<C-h>", "<esc><C-w>h", opts)
-vim.keymap.set("i", "<C-j>", "<esc><C-w>j", opts)
-vim.keymap.set("i", "<C-k>", "<esc><C-w>k", opts)
-vim.keymap.set("i", "<C-l>", "<esc><C-w>l", opts)
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -130,6 +130,12 @@ return {
         pattern = { "dap-repl", "dap*" },
         callback = function()
           vim.opt_local.cursorline = false
+
+          local opts = { noremap = true, silent = true, buffer = true }
+          vim.keymap.set("i", "<C-h>", "<esc><C-w>h", opts)
+          vim.keymap.set("i", "<C-j>", "<esc><C-w>j", opts)
+          vim.keymap.set("i", "<C-k>", "<esc><C-w>k", opts)
+          vim.keymap.set("i", "<C-l>", "<esc><C-w>l", opts)
         end,
       })
     end,
