7cf8c2ffb88c60ce2058db7c712f6f29faf5debb
m00p1ng <mongkonchai4412@gmail.com>
2025-01-17 22:35:42 +0700

    add http

--- after/ftplugin/http.lua
+++ after/ftplugin/http.lua
@@ -4,10 +4,4 @@ end
 
 require("which-key").add {
   { "<leader>m", group = "HTTP" },
-  { "<leader>mr", "<cmd>lua require('kulala').run()<cr>", desc = "Send Request", buffer = true },
-  { "<leader>ml", "<cmd>lua require('kulala').replay()<cr>", desc = "Replay", buffer = true },
-  { "<leader>mf", "<cmd>lua require('kulala').from_curl()<cr>", desc = "From cURL", buffer = true },
-  { "<leader>mc", "<cmd>lua require('kulala').copy()<cr>", desc = "To cURL", buffer = true },
-  { "[r", "<cmd>lua require('kulala').jump_prev()<cr>", desc = "Prev req", buffer = true },
-  { "]r", "<cmd>lua require('kulala').jump_next()<cr>", desc = "Next req", buffer = true },
 }
