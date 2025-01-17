81ff84975e9b1991e813e10ea8dcf95ab391d08b
m00p1ng <mongkonchai4412@gmail.com>
2025-01-17 22:14:08 +0700

    update treewalker

--- lua/plugins/treewalker.lua
+++ lua/plugins/treewalker.lua
@@ -30,11 +30,42 @@ return {
       end
     end
 
+    local function swap_vertical(options)
+      return function()
+        require("demicolon.jump").repeatably_do(function(opts)
+          local direction = (opts.forward == nil or opts.forward)
+          if direction then
+            require("treewalker").swap_up()
+          else
+            require("treewalker").swap_down()
+          end
+        end, options)
+      end
+    end
+
+    local function swap_horizontal(options)
+      return function()
+        require("demicolon.jump").repeatably_do(function(opts)
+          local direction = (opts.forward == nil or opts.forward)
+          if direction then
+            require("treewalker").swap_right()
+          else
+            require("treewalker").swap_left()
+          end
+        end, options)
+      end
+    end
+
     return {
-      { "]k", jump_neighbor { forward = true }, mode = { "n", "v" }, desc = "Move down to the next neighbor node" },
-      { "[k", jump_neighbor { forward = false }, mode = { "n", "v" }, desc = "Move up to the next neighbor node" },
-      { "[p", jump_parent { forward = true }, mode = { "n", "v" }, desc = "Finds the next good parent node" },
-      { "]p", jump_parent { forward = false }, mode = { "n", "v" }, desc = "Finds the next good child node" },
+      { "[k", jump_neighbor { forward = false }, mode = { "n", "v" }, desc = "Move up node" },
+      { "]k", jump_neighbor { forward = true }, mode = { "n", "v" }, desc = "Move down node" },
+      { "]p", jump_parent { forward = false }, mode = { "n", "v" }, desc = "Move child node" },
+      { "[p", jump_parent { forward = true }, mode = { "n", "v" }, desc = "Move parent node" },
+
+      { "[K", swap_vertical { forward = false }, desc = "Swap node up" },
+      { "]K", swap_vertical { forward = true }, desc = "Swap node down" },
+      { "[P", swap_horizontal { forward = false }, desc = "Swap node left" },
+      { "]P", swap_horizontal { forward = true }, desc = "Swap node right" },
     }
   end,
 }
