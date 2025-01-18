590eb99dcf728f4cfbde7e9a0424f27e86269279
m00p1ng <mongkonchai4412@gmail.com>
2025-01-18 12:20:28 +0700

    remove auto-indent

--- lua/plugins/auto-indent.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "vidocqh/auto-indent.nvim",
-  event = "InsertEnter",
-  opts = {
-    lightmode = true, -- Lightmode assumes tabstop and indentexpr not change within buffer's lifetime
-    indentexpr = nil, -- Use vim.bo.indentexpr by default, see 'Custom Indent Evaluate Method'
-    ignore_filetype = {}, -- Disable plugin for specific filetypes,
-  },
-}
