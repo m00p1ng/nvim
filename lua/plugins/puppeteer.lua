63cd1d216e2c17a0dff33b2a61279c9b4346a16b
m00p1ng <mongkonchai4412@gmail.com>
2025-01-24 23:15:44 +0700

    update

--- lua/plugins/puppeteer.lua
+++ lua/plugins/puppeteer.lua
@@ -1,4 +1,7 @@
 return {
   "chrisgrieser/nvim-puppeteer",
   lazy = false, -- plugin lazy-loads itself. Can also load on filetypes.
+  init = function()
+    vim.g.puppeteer_disable_filetypes = require("utils").ui_filetypes
+  end,
 }
