ca59b6223bea92e96d0a8ca69a9b3f5b9c38456f
m00p1ng <mongkonchai4412@gmail.com>
2025-01-30 23:22:45 +0700

    add listchars

--- lua/plugins/snacks/toggle.lua
+++ lua/plugins/snacks/toggle.lua
@@ -7,5 +7,20 @@ return {
     Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map "<leader>ob"
     Snacks.toggle.profiler():map "<leader>op"
     Snacks.toggle.inlay_hints():map "<leader>oi"
+
+    Snacks.toggle({
+      name = "List Chars",
+      get = function()
+        return vim.opt.list:get()
+      end,
+      set = function(state)
+        vim.opt.list = state
+        if state then
+          Snacks.indent.disable()
+        else
+          Snacks.indent.enable()
+        end
+      end,
+    }):map "<leader>oc"
   end,
 }
