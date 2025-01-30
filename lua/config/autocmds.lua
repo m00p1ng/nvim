db575a562c2731f85c99d289139fce7cedf87565
m00p1ng <mongkonchai4412@gmail.com>
2025-01-31 02:10:54 +0700

    add git keymap

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -97,3 +97,19 @@ vim.api.nvim_create_autocmd("User", {
     require("utils.winbar").create_winbar()
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("git_keymap", { clear = true }),
+  pattern = "gitcommit",
+  callback = function()
+    vim.keymap.set("i", "<C-s>", function()
+      local result = vim.system({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, { text = true }):wait()
+      if result.code ~= 0 then
+        vim.notify("Git repository not found", vim.log.levels.WARN)
+        return
+      end
+      local branch = vim.trim(result.stdout)
+      vim.api.nvim_feedkeys(branch, "i", false)
+    end, { expr = true })
+  end,
+})
