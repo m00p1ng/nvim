f9eb2795545098fd3277e1641e50b015f15b9227
m00p1ng <mongkonchai4412@gmail.com>
2025-02-07 10:23:58 +0700

    add auto detected vue version

--- lua/plugins/lang/vue.lua
+++ lua/plugins/lang/vue.lua
@@ -1,3 +1,35 @@
+vim.api.nvim_create_autocmd("UIEnter", {
+  group = vim.api.nvim_create_augroup("set_vue_version", { clear = true }),
+  callback = function()
+    if vim.g.vscode or vim.g.vue_version then
+      return
+    end
+
+    if vim.fn.filereadable "package.json" == 0 then
+      return
+    end
+
+    vim.system(
+      { "rg", '"vue": "[^\\w\\d]*(\\d+\\.\\d+\\.\\d+)"', "-or", "$1", "package.json" },
+      { text = true },
+      function(out)
+        if out.code ~= 0 then
+          return
+        end
+
+        local version = vim.trim(out.stdout)
+
+        print("Detected vue.js v" .. version)
+        if vim.version.ge(version, "2.7.0") then
+          vim.g.vue_version = 3
+        else
+          vim.g.vue_version = 2
+        end
+      end
+    )
+  end,
+})
+
 return {
   -- depends on the typescript extra
   { import = "plugins.lang.typescript" },
