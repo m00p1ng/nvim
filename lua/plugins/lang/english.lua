b1200af967cec02552d9e3259dce005846d90426
m00p1ng <mongkonchai4412@gmail.com>
2025-04-14 21:11:48 +0700

    update rule harper

--- lua/plugins/lang/english.lua
+++ lua/plugins/lang/english.lua
@@ -7,6 +7,7 @@ return {
           settings = {
             ["harper-ls"] = {
               linters = {
+                LongSentences = false,
                 SentenceCapitalization = false,
                 SpellCheck = false,
               },
