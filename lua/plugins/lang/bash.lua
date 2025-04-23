9a01b1a4f6390d971d0e459eb622dd18271bd2c0
m00p1ng <mongkonchai4412@gmail.com>
2025-04-23 21:52:27 +0700

    update bash

--- lua/plugins/lang/bash.lua
+++ lua/plugins/lang/bash.lua
@@ -12,4 +12,17 @@ return {
     opts_extend = { "ensure_installed" },
     opts = { ensure_installed = { "bash" } },
   },
+  {
+    "williamboman/mason.nvim",
+    opts_extend = { "ensure_installed" },
+    opts = { ensure_installed = { "shfmt", "shellcheck" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sh = { "shfmt" },
+      },
+    },
+  },
 }
