ee1b7a77f079d81b0709b7922d7e702a9d1817a2
m00p1ng <mongkonchai4412@gmail.com>
2025-05-08 21:52:32 +0700

    refactor .env

--- lua/plugins/lang/env.lua
+++ lua/plugins/lang/env.lua
@@ -1,42 +1,3 @@
-local last_env = nil
-
-local function select_env()
-  local result = vim
-    .system({ "fd", "-t", "f", "--no-ignore-vcs", "--hidden", "^\\.env\\..*|.*\\.env$" }, { text = true })
-    :wait()
-  if result.code ~= 0 then
-    vim.notify("Cannot find .env, please try again later", vim.log.levels.WARN)
-    return
-  end
-
-  local ret = vim.trim(result.stdout)
-  if ret == "" then
-    vim.notify(".env was not found", vim.log.levels.WARN)
-    return
-  end
-
-  local envs = vim.split(ret, "\n")
-  vim.ui.select(envs, {
-    prompt = "Select environment:",
-  }, function(choice)
-    if not choice then
-      return
-    end
-    vim.cmd.Dotenv(choice)
-    vim.notify("Loaded " .. choice, vim.log.levels.INFO)
-    last_env = choice
-  end)
-end
-
-local function reload_env()
-  if last_env then
-    vim.cmd.Dotenv(last_env)
-    vim.notify("Reloaded " .. last_env, vim.log.levels.INFO)
-  else
-    select_env()
-  end
-end
-
 return {
   {
     "ellisonleao/dotenv.nvim",
@@ -50,9 +11,50 @@ return {
       })
     end,
     opts = {},
-    keys = {
-      { "<leader>V", select_env, desc = "Dotenv: Load" },
-      { "<leader>v", reload_env, desc = "Dotenv: Reload" },
-    },
+    keys = function()
+      local last_env = nil
+
+      local function select_env()
+        local result = vim
+          .system({ "fd", "-t", "f", "--no-ignore-vcs", "--hidden", "^\\.env\\..*|.*\\.env$" }, { text = true })
+          :wait()
+        if result.code ~= 0 then
+          vim.notify("Cannot find .env, please try again later", vim.log.levels.WARN)
+          return
+        end
+
+        local ret = vim.trim(result.stdout)
+        if ret == "" then
+          vim.notify(".env was not found", vim.log.levels.WARN)
+          return
+        end
+
+        local envs = vim.split(ret, "\n")
+        vim.ui.select(envs, {
+          prompt = "Select environment:",
+        }, function(choice)
+          if not choice then
+            return
+          end
+          vim.cmd.Dotenv(choice)
+          vim.notify("Loaded " .. choice, vim.log.levels.INFO)
+          last_env = choice
+        end)
+      end
+
+      local function reload_env()
+        if last_env then
+          vim.cmd.Dotenv(last_env)
+          vim.notify("Reloaded " .. last_env, vim.log.levels.INFO)
+        else
+          select_env()
+        end
+      end
+
+      return {
+        { "<leader>V", select_env, desc = "DotENV: Load" },
+        { "<leader>v", reload_env, desc = "DotENV: Reload" },
+      }
+    end,
   },
 }
