43f7f293fae9d4b46762b1da40f3c49923f2bf2d
m00p1ng <mongkonchai4412@gmail.com>
2025-08-19 14:45:19 +0700

    update copilot

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -15,7 +15,7 @@ return {
           open = "<M-CR>",
         },
         layout = {
-          position = "bottom", -- | top | left | right | horizontal | vertical
+          position = "bottom", -- | top | left | right | bottom |
           ratio = 0.4,
         },
       },
@@ -24,6 +24,7 @@ return {
         auto_trigger = true,
         hide_during_completion = true,
         debounce = 75,
+        trigger_on_accept = true,
         keymap = {
           accept = "<c-f>",
           accept_word = false,
@@ -44,7 +45,39 @@ return {
         cvs = false,
         ["."] = false,
       },
-      copilot_node_command = "node", -- Node.js version must be > 18.x
+      auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
+      logger = {
+        file = vim.fn.stdpath "log" .. "/copilot-lua.log",
+        file_log_level = vim.log.levels.OFF,
+        print_log_level = vim.log.levels.WARN,
+        trace_lsp = "off", -- "off" | "messages" | "verbose"
+        trace_lsp_progress = false,
+        log_lsp_messages = false,
+      },
+      copilot_node_command = "node", -- Node.js version must be > 20
+      workspace_folders = {},
+      copilot_model = "",
+      disable_limit_reached_message = false, -- Set to `true` to suppress completion limit reached popup
+      root_dir = function()
+        return vim.fs.dirname(vim.fs.find(".git", { upward = true })[1])
+      end,
+      -- should_attach = function(_, _)
+      --   if not vim.bo.buflisted then
+      --     logger.debug "not attaching, buffer is not 'buflisted'"
+      --     return false
+      --   end
+      --
+      --   if vim.bo.buftype ~= "" then
+      --     logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
+      --     return false
+      --   end
+      --
+      --   return true
+      -- end,
+      server = {
+        type = "nodejs", -- "nodejs" | "binary"
+        custom_server_filepath = nil,
+      },
       server_opts_overrides = {},
     },
   },
