2621a86cd47d985b8584225ff73c4cf37c9befa0
m00p1ng <mongkonchai4412@gmail.com>
2025-10-10 13:30:37 +0700

    update

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,7 +5,6 @@ return {
     "mfussenegger/nvim-dap",
     -- use `config` instead of `opts` or `init` to avoid bugs
     config = function()
-      vim.api.nvim_set_hl(0, "DapStopped", { bg = "#4B4B18" })
       vim.api.nvim_set_hl(0, "DapStoppedText", { fg = "yellow" })
 
       vim.fn.sign_define("DapBreakpoint", {
