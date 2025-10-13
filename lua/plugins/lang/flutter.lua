8204ecb4b01de5a75cc9c9f984f83fd6302f6868
m00p1ng <mongkonchai4412@gmail.com>
2025-10-13 21:58:33 +0700

    update flutter

--- lua/plugins/lang/flutter.lua
+++ lua/plugins/lang/flutter.lua
@@ -9,13 +9,6 @@ return {
   {
     "nvim-flutter/flutter-tools.nvim",
     ft = "dart",
-    init = function()
-      require("which-key").add {
-        { "<leader>m", group = "flutter" },
-        { "<leader>mt", "<cmd>telescope flutter commands<cr>", desc = "menu", buffer = true },
-        { "<leader>mf", "<cmd>telescope flutter fvm<cr>", desc = "fvm", buffer = true },
-      }
-    end,
     opts = {
       ui = {
         -- the border type to use for all floating windows, the same options/formats
@@ -42,31 +35,45 @@ return {
       },
       debugger = { -- integrate with nvim dap + install dart code debugger
         enabled = false,
-        run_via_dap = false, -- use dap instead of a plenary job to run flutter apps
         -- if empty dap will not stop on any exceptions, otherwise it will stop on those specified
         -- see |:help dap.set_exception_breakpoints()| for more info
         exception_breakpoints = {},
+        -- Whether to call toString() on objects in debug views like hovers and the
+        -- variables list.
+        -- Invoking toString() has a performance cost and may introduce side-effects,
+        -- although users may expected this functionality. null is treated like false.
+        evaluate_to_string_in_debug_views = true,
+        -- You can use the `debugger.register_configurations` to register custom runner configuration (for example for different targets or flavor). Plugin automatically registers the default configuration, but you can override it or add new ones.
         -- register_configurations = function(paths)
         --   require("dap").configurations.dart = {
-        --     <put here config that you would find in .vscode/launch.json>
+        --     -- your custom configuration
         --   }
         -- end,
       },
       -- flutter_path = "<full/path/if/needed>", -- <-- this takes priority over the lookup
       flutter_lookup_cmd = nil, -- example "dirname $(which flutter)" or "asdf where flutter"
+      root_patterns = { ".git", "pubspec.yaml" }, -- patterns to find the root of your flutter project
       fvm = true, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
+      default_run_args = nil, -- Default options for run command (i.e `{ flutter = "--no-version-check" }`). Configured separately for `dart run` and `flutter run`.
       widget_guides = {
         enabled = true,
       },
       closing_tags = {
         highlight = "NonText", -- highlight for the closing tag
         prefix = "> ", -- character to use for close tag e.g. > Widget
+        priority = 10, -- priority of virtual text in current line
+        -- consider to configure this when there is a possibility of multiple virtual text items in one line
+        -- see `priority` option in |:help nvim_buf_set_extmark| for more info
         enabled = true, -- set to false to disable
       },
       dev_log = {
         enabled = true,
+        filter = nil, -- optional callback to filter the log
+        -- takes a log_line as string argument; returns a boolean or nil;
+        -- the log_line is only added to the output if the function returns true
         notify_errors = false, -- if there is an error whilst running then notify the user
         open_cmd = "10sp", -- command to use to open the log buffer
+        focus_on_open = true, -- focus on the newly opened log window
       },
       dev_tools = {
         autostart = false, -- autostart devtools server if not detected
@@ -80,13 +87,18 @@ return {
         color = { -- show the derived colours for dart variables
           enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
           background = true, -- highlight the background
-          background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 20, g = 17, b = 24},)
+          background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
           foreground = false, -- highlight the foreground
           virtual_text = false, -- show the highlight using virtual text
           virtual_text_str = "■", -- the virtual text character to highlight
         },
-        on_attach = require("plugins.lsp.keymaps").on_attach,
-        capabilities = require("plugins.lsp.keymaps").capabilities, -- e.g. lsp_status capabilities
+        -- on_attach = my_custom_on_attach,
+        -- capabilities = my_custom_capabilities, -- e.g. lsp_status capabilities
+        --- OR you can specify a function to deactivate or change or control how the config is created
+        -- capabilities = function(config)
+        --   config.specificThingIDontWant = false
+        --   return config
+        -- end,
         -- see the link below for details on each option:
         -- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
         settings = {
